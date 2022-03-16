ec7bff05fc6c35ed2dd37c8ef53a0be39a904343
LUCKSTUDITY <sinhastudent10@gmail.com>
2022-03-16 14:44:30 +0530

    Fourth commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width:{
+        '128px': '90%',
+      }
+      // height and borders editing tailwind config file will be discussed in later videos.
+    },
+  },
+  plugins: [],
+}
